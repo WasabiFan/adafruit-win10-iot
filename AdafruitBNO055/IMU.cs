79a3bc3b85be54e2c5c1911806f413494c76342a
WasabiFan <wasabifan@outlook.com>
2016-12-28 13:39:11 -0500

    Initial port of code from online implementation
    There is no added functionality yet, only major refactoring. The implementation that this is based on was extremely badly-written so there are some large changes in structure here.

---
+++ AdafruitBNO055/IMU.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Devices.I2c;
+
+namespace Adafruit.BNO055
+{
+    public class BNO055IMU
+    {
+        private readonly AddressMode IMUAddress;
+        private readonly string I2CFriendlyName;
+        private const byte BNO055_ID = 0xA0;
+
+        I2cDevice DeviceConnection = null;
+        Timer IMUTimer = null;
+
+        public BNO055IMU(string i2cFriendlyName = "I2C1", AddressMode imuAddress = AddressMode.AddressA)
+        {
+            this.I2CFriendlyName = i2cFriendlyName;
+            this.IMUAddress = imuAddress;
+        }
+
+        public async void Initialize(int numRetries = 1, int retryDelay = 1000)
+        {
+            var i2cSettings = new I2cConnectionSettings((byte)IMUAddress);
+            i2cSettings.BusSpeed = I2cBusSpeed.StandardMode;
+
+            string DeviceSelector = I2cDevice.GetDeviceSelector(I2CFriendlyName);
+            var I2CDeviceControllers = await DeviceInformation.FindAllAsync(DeviceSelector);
+            if (I2CDeviceControllers.Count <= 0)
+                throw new Exception("No valid I2C controllers were found");
+
+            DeviceConnection = await I2cDevice.FromIdAsync(I2CDeviceControllers[0].Id, i2cSettings);
+
+            for (int AttemptIndex = 0; ReadByte(BNO055Register.BNO055_CHIP_ID_ADDR) != BNO055_ID; AttemptIndex++)
+            {
+                await Task.Delay(retryDelay);
+                if (AttemptIndex >= numRetries)
+                    throw new Exception("The IMU didn't properly respond over the I2C bus. Confirm that it is properly"
+                        + " connected and that the supplied I2C bus name is correct.");
+            }
+
+            // This is the default mode, but setting it manually ensures proper operation
+            await SetMode(OperationMode.Config);
+
+            // Reset the sensor
+            WriteByte(BNO055Register.BNO055_SYS_TRIGGER_ADDR, 0x20);
+            await Task.Delay(1000);
+            while (ReadByte(BNO055Register.BNO055_CHIP_ID_ADDR) != BNO055_ID)
+            {
+                await Task.Delay(40);
+            }
+            await Task.Delay(50);
+
+            /* Set to normal power mode */
+            WriteByte(BNO055Register.BNO055_PWR_MODE_ADDR, (byte)PowerMode.Normal);
+            await Task.Delay(10);
+
+            WriteByte(BNO055Register.BNO055_PAGE_ID_ADDR, 0);
+
+            /* Set the output units */
+            /*
+            uint8_t unitsel = (0 << 7) | // Orientation = Android
+                              (0 << 4) | // Temperature = Celsius
+                              (0 << 2) | // Euler = Degrees
+                              (1 << 1) | // Gyro = Rads
+                              (0 << 0);  // Accelerometer = m/s^2
+            write8(BNO055_UNIT_SEL_ADDR, unitsel);
+            */
+            WriteByte((byte)BNO055Register.BNO055_SYS_TRIGGER_ADDR, 0x0);
+            await Task.Delay(10);
+
+            /* Set the requested operating mode (see section 3.3) */
+            await SetMode(OperationMode.NineDegsOfFreedom);
+            await Task.Delay(10);
+        }
+
+        private void AssertConnected()
+        {
+            if (DeviceConnection == null)
+                throw new InvalidOperationException("An attempt was made to communicate with the sensor before the"
+                    + $" sensor was initialized. Call {nameof(Initialize)}() before reading or writing data.");
+        }
+
+        public BNO055IMUReading GetNewReading()
+        {
+            return new BNO055IMUReading(
+                ReadCalibration(),
+                ReadTemp(),
+                ReadVector(IMUVectorType.Accelerometer),
+                ReadVector(IMUVectorType.Euler),
+                ReadVector(IMUVectorType.Gravity),
+                ReadVector(IMUVectorType.Gyroscope),
+                ReadVector(IMUVectorType.LinearAccel),
+                ReadVector(IMUVectorType.Magnetometer));
+
+        }
+
+        public CalibrationData ReadCalibration()
+        {
+            return new CalibrationData(ReadByte((byte)BNO055Register.BNO055_CALIB_STAT_ADDR));
+        }
+
+        private async Task SetMode(OperationMode mode)
+        {
+            WriteByte((byte)BNO055Register.BNO055_OPR_MODE_ADDR, (byte)mode);
+            await Task.Delay(30);
+        }
+
+        public int ReadTemp()
+        {
+            return ReadByte((byte)BNO055Register.BNO055_TEMP_ADDR);
+        }
+
+        public Vector ReadVector(IMUVectorType type)
+        {
+            AssertConnected();
+
+            byte[] I2CBuffer = new byte[6];
+            DeviceConnection.Write(new byte[] { (byte)type });
+            DeviceConnection.Read(I2CBuffer);
+
+            Vector RawVector = new Vector(
+                I2CBuffer[0] | (I2CBuffer[1] << 8),
+                I2CBuffer[2] | (I2CBuffer[3] << 8),
+                I2CBuffer[4] | (I2CBuffer[5] << 8));
+
+            /* Convert the value to an appropriate range (section 3.6.4) */
+            /* and assign the value to the Vector type */
+
+            switch (type)
+            {
+                case IMUVectorType.Magnetometer:
+                    /* 1uT = 16 LSB */
+                    return RawVector / 16.0;
+                case IMUVectorType.Gyroscope:
+                    /* 1rps = 900 LSB */
+                    return RawVector / 900.0;
+                case IMUVectorType.Euler:
+                    /* 1 degree = 16 LSB */
+                    return RawVector / 16.0;
+                case IMUVectorType.Accelerometer:
+                case IMUVectorType.LinearAccel:
+                case IMUVectorType.Gravity:
+                    /* 1m/s^2 = 100 LSB */
+                    return RawVector / 100.0;
+            }
+
+            throw new ArgumentException($"Invalid vector type {type}", nameof(type));
+        }
+
+        public byte ReadByte(byte addr)
+        {
+            AssertConnected();
+
+            byte[] I2CBuffer = new byte[20];
+            DeviceConnection.Write(new byte[] { addr });
+            DeviceConnection.Read(I2CBuffer);
+            return I2CBuffer[0];
+        }
+
+        public byte ReadByte(BNO055Register register)
+        {
+            return ReadByte((byte)register);
+        }
+
+        public void WriteByte(byte addr, byte data)
+        {
+            AssertConnected();
+
+            byte[] I2CBuffer = new byte[2];
+            I2CBuffer[0] = addr;
+            I2CBuffer[1] = data;
+            DeviceConnection.Write(I2CBuffer);
+        }
+
+        public void WriteByte(BNO055Register register, byte data)
+        {
+            WriteByte((byte)register, data);
+        }
+    }
+}
---
+++ AdafruitBNO055/IMUReading.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adafruit.BNO055
+{
+    public struct CalibrationData
+    {
+        public CalibrationData(byte calibrationData)
+        {
+            System = (ushort)((calibrationData >> 6) & 0x03);
+            Gyro = (ushort)((calibrationData >> 4) & 0x03);
+            Accel = (ushort)((calibrationData >> 2) & 0x03);
+            Mag = (ushort)(calibrationData & 0x03);
+        }
+
+        public readonly ushort System;
+        public readonly ushort Gyro;
+        public readonly ushort Accel;
+        public readonly ushort Mag;
+    }
+
+    public struct Vector
+    {
+        public Vector(double x, double y, double z)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Z = z; 
+        }
+
+        public double X;
+        public double Y;
+        public double Z;
+
+        public static Vector operator +(Vector a, Vector b)
+        {
+            return new Vector(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+        }
+
+        public static Vector operator -(Vector a, Vector b)
+        {
+            return new Vector(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
+        }
+
+        public static Vector operator *(Vector a, Vector b)
+        {
+            return new Vector(a.X * b.X, a.Y * b.Y, a.Z * b.Z);
+        }
+
+        public static Vector operator *(Vector a, double c)
+        {
+            return new Vector(a.X * c, a.Y * c, a.Z * c);
+        }
+
+        public static Vector operator /(Vector a, Vector b)
+        {
+            return new Vector(a.X / b.X, a.Y / b.Y, a.Z / b.Z);
+        }
+
+        public static Vector operator /(Vector a, double c)
+        {
+            return new Vector(a.X / c, a.Y / c, a.Z / c);
+        }
+    }
+
+    public struct BNO055IMUReading
+    {
+        public BNO055IMUReading(
+            CalibrationData calibration,
+            int temperature,
+            Vector acceleration,
+            Vector eulerRotation,
+            Vector gravity,
+            Vector gyro,
+            Vector linearAcceleration,
+            Vector magnetometer)
+        {
+            this.Calibration = calibration;
+            this.Temperature = temperature;
+            this.Acceleration = acceleration;
+            this.EulerRotation = eulerRotation;
+            this.Gravity = gravity;
+            this.Gyro = gyro;
+            this.LinearAcceleration = linearAcceleration;
+            this.Magnetometer = magnetometer;
+        }
+
+        public readonly CalibrationData Calibration;
+
+        public readonly int Temperature;
+        public readonly Vector Acceleration;
+        public readonly Vector EulerRotation;
+        public readonly Vector Gravity;
+        public readonly Vector Gyro;
+        public readonly Vector LinearAcceleration;
+        public readonly Vector Magnetometer;
+    }
+}
